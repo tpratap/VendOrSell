a2be5e6ec9608c97c4499bbe3d32a990dd661c8d
tpratap <tejpartap957@gmail.com>
2022-06-06 21:07:39 +0530

    pagechange

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 	],
     # 'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 10
 }
 
 AUTH_USER_MODEL = 'accounts.MyUser'
